e1a1f8657e7e195a9a402f8be7a4d77c1cd016b9
liujiangbo01 <liujiangbo01@sunlands.com>
2018-08-15 17:41:45 +0800

    FIRST CONMMIT

---
+++ supershop-back/src/main/java/com/leyo/supershopback/SupershopBackApplication.java
@@ -0,0 +1,12 @@
+package com.leyo.supershopback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupershopBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupershopBackApplication.class, args);
+	}
+}
---
+++ supershop-front/src/main/java/com/leyo/supershopfront/SupershopFrontApplication.java
@@ -0,0 +1,12 @@
+package com.leyo.supershopfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupershopFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupershopFrontApplication.class, args);
+	}
+}
